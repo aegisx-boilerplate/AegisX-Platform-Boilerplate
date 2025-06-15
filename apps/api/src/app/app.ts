babb078d0e99c28789bf6e6e5681a7ad77af21ea
Sathit Seethaphon <dixonsatit@gmail.com>
2025-06-15 16:14:47 +0700

    feat: แก้ไข core libs ทั้งหมดให้เป็น CommonJS และทดสอบ import ใน API - ลบ type: module ออกจาก package.json ของ core libs ทั้งหมด - เพิ่ม module: commonjs และ moduleResolution: node ใน tsconfig.lib.json - เพิ่มการทดสอบ import core libs ทั้งหมดใน API - แก้ปัญหา ESM/CJS interop ใน monorepo

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -2,13 +2,25 @@ import * as path from 'path';
 import { FastifyInstance } from 'fastify';
 import AutoLoad from '@fastify/autoload';
 import { coreLogger } from '@aegisx/core-logger';
+import { coreAuth } from '@aegisx/core-auth';
+import { coreConfig } from '@aegisx/core-config';
+import { coreDatabase } from '@aegisx/core-database';
+import { coreErrors } from '@aegisx/core-errors';
+import { coreRbac } from '@aegisx/core-rbac';
 
 /* eslint-disable-next-line */
 export interface AppOptions { }
 
 export async function app(fastify: FastifyInstance, opts: AppOptions) {
   // Place here your custom code!
-  console.log('Test coreLogger:', coreLogger());
+  console.log('=== Testing All Core Libraries ===');
+  console.log('coreLogger:', coreLogger());
+  console.log('coreAuth:', coreAuth());
+  console.log('coreConfig:', coreConfig());
+  console.log('coreDatabase:', coreDatabase());
+  console.log('coreErrors:', coreErrors());
+  console.log('coreRbac:', coreRbac());
+  console.log('=== All Core Libraries Working! ===');
 
   // Do not touch the following lines
 
